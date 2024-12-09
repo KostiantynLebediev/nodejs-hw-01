03743fee045dd567256bd3012a620a02e89b1961
KostiantynLebediev <lebedievkostiantyn@gmail.com>
2024-12-09 16:18:49 +0000

    getAll

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,5 +1,12 @@
 import { readContacts } from "../utils/readContacts";
 
-export const getAllContacts = async () => readContacts();
+export const getAllContacts = async () => {
+    try {
+      const res = await readContacts();
+      return res;
+    } catch (e) {
+      console.error(e.message);
+    }
+  };
 
 console.log(await getAllContacts());
--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,8 +1,12 @@
-import { readFile } from 'node:fs/promises';
+import fs from "node:fs/promises";
 
 import { PATH_DB } from '../constants/contacts.js';
 
 export const readContacts = async () => {
-  const data = await readFile(PATH_DB, 'utf-8');
-  return JSON.parse(data);
-};
+    try {
+      const buffer = await fs.readFile(PATH_DB);
+      return buffer.toString("utf-8");
+    } catch (e) {
+      console.error(e.message);
+    }
+  };
